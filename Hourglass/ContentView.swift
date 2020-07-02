09fa41ef8f34f666efcce9da1643d9748c6211dd
Richard Robinson <robinson.ian.richard@gmail.com>
2020-07-02 01:12:29 -0400

    Initial Commit

---
+++ Hourglass/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hourglass
+//
+//  Created by Richard Robinson on 2020-07-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hourglass/HourglassApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourglassApp.swift
+//  Hourglass
+//
+//  Created by Richard Robinson on 2020-07-02.
+//
+
+import SwiftUI
+
+@main
+struct HourglassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
